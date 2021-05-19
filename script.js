1bf3216359312c55c3e3a9b96b95e07b43463030
Mishaproh96 <mishaprohorenko96@gmail.com>
2021-05-19 20:17:40 +0300

    123

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 'use strict';
 
+const body = document.body;
+
+// console.log(body);
+// console.dir(body)
+// console.log(body.childNodes)
+
+const childNodes = body.childNodes;
+// let arr = []
+// for (let i = 0; i < childNodes.length; i++) {
+    // arr.push(childNodes[i])
+// console.dir(childNodes[i])
+// }
+// console.dir(arr)
+
+// for(let node of childNodes){
+    // console.dir(node)
+// }
+
+const children = body.children;
+for (let i = 0; i < children.length; i++) {
+    if(children[i].innerText == 'Узел'){
+        children[i].innerText = 'aaaaaa'
+    }     
+ };
+
+// console.log(children)
 
